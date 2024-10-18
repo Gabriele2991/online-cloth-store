734754544b2db9aebb6a1e6c95eb61944a6885d3
Gabriele Mangiavacchi <ninroot90@gmail.com>
2024-10-18 11:48:59 +0200

    added subtract and add functions to arrow in quantity span

--- src/components/checkout-items/checkout-item.component.jsx
+++ src/components/checkout-items/checkout-item.component.jsx
@@ -5,18 +5,21 @@ import "./checkout-item.styles.scss"
 
 const CheckoutItem = ({cartItem})=>{
     const{name, imageUrl,price,quantity}= cartItem;
-    const {removeItemFromCart} = useContext(CartContext)
+    const {addItemToCart,subtractItemFromCart,removeItemFromCart} = useContext(CartContext)
 
-    const removeItemHandler = ()=>removeItemFromCart(cartItem);
     return (
         <div className="checkout-item-container">
             <div className="image-container">
                 <img src={imageUrl} alt={`${name}`} />
             </div>
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={()=>subtractItemFromCart(cartItem)}>&#10094;</div>
+                <span className="value">{quantity}</span>
+                <div className="arrow" onClick={()=>addItemToCart(cartItem)}>&#10095;</div>
+            </span>
             <span className="price">${price}</span>
-            <div className="remove-button" onClick={removeItemHandler}>&#10005;</div>
+            <div className="remove-button" onClick={()=>removeItemFromCart(cartItem)}>&#10005;</div>
         </div>
     )
 }
