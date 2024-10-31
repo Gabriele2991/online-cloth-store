f7b63121b3be42f0adde826668269140584806f8
Gabriele Mangiavacchi <ninroot90@gmail.com>
2024-10-31 17:56:14 +0100

    commit before halloween

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -2,23 +2,23 @@ import { createContext, useState, useEffect } from "react";
 
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-export const ProductsContext = createContext({
-    products: [],
+export const CategoriesContext = createContext({
+    categoriesMap: {},
 });
 
-export const ProductsProvider = ({children})=>{
-    const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({children})=>{
+    const [categoriesMap, setCategoriesMap] = useState({});
 
     useEffect(()=>{
         const getCategoriesMap = async ()=>{
             const categoryMap = await getCategoriesAndDocuments();
             console.log("Category Map in productContext_useEffect: "+ {...categoryMap});
+            setCategoriesMap(categoryMap);
         };
-
         getCategoriesMap();
-    },[])
+    }, []);
 
-    const value = {products};
-     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    const value = {categoriesMap};
+     return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
 }
 
