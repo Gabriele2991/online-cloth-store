9a2ea1a50e47bfa6603c31c553b93dce7c3cad7c
Gabriele Mangiavacchi <ninroot90@gmail.com>
2024-11-22 10:48:59 +0100

    updated redirects for netlify

--- src/components/directory-items/directory-item.components.jsx
+++ src/components/directory-items/directory-item.components.jsx
@@ -1,9 +1,19 @@
 import {BackgroundImage, Body, DirectoryItemContainer} from "./directory-item.styles";
-
+//import { useNavigate } from "react-router-dom";
 const DirectoryItem = ({category}) =>{
-    const {title, imageUrl} = category;
+    
+    //I used the way to change the Body in a LINK in the stylesheet BUT
+    //we can use the HOOK useNavigate, passing the created route in the document
+    //that handle the images in homepage in JsonDirectory and use it in useNavigate
+
+    const {title, imageUrl, /*route*/ } = category;
+
+    // const navigate = useNavigate();
+    // const onNavigateHandler = ()=>navigate(route;)
+
     return(
-    <DirectoryItemContainer>
+    
+    <DirectoryItemContainer /*onClick={onNavigateHandler}*/>
         <BackgroundImage imageUrl={imageUrl} />
         <Body to={"shop/" + title.toLowerCase()}>
             <h2>{title}</h2>
